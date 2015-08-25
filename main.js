d8a6cd2bb439982a4cc1dcc54ac5b02471258e21
Kathleen Kent <lashkath@gmail.com>
2015-08-25 15:07:46 -0700

    added updated main.js

--- main.js
+++ main.js
@@ -8,3 +8,42 @@ var game = new Grid(testStr);
 
 var viewer = new Viewer(game);
 viewer.show();
+
+
+// starting template for sudoku solver
+// IIFE's?
+
+//DigitSet constructor
+var DigitSet = (function(squareID) {
+	var num_array = [1,2,3,4,5,6,7,8,9];
+	//DigitSet.makeSquare = 
+	//DigitSet.makeBlock = 
+	//DigitSet.makeGrid = 
+})();
+
+
+// var ___ = require('lodash')
+// module.exports('DigitSet')
+// Grid constructor
+var Grid = (function(row, column, /*key? row and column index*/) {
+	//if (squareID === undefined) {
+		//DigitSet();
+	//} else {
+		// return squareID value ? row[0] && column[0]
+		// 
+	//};
+})();
+
+//Viewer constructor
+//See sudoku_viewer.js for completed grid
+var Viewer = (function(/*parameter?*/) {
+	var rowCounter = [0,1,2,3,4,5,6,7,8,9];
+	if(rowCount % 3 == 0) {
+		console.log("+-----------+-----------+-----------+");
+	} else {
+		// if row[i] === undefined console.log(" " not undefined);
+		console.log('|' + row[0] + '|' + row[1] + '|' + row[2] + '|' +
+			row[3] + '|' + row[4] + '|' + row[5] + '|' + row[6] + '|' +
+		    row[7] + '|' + row[8] + '|');
+	}
+})();
